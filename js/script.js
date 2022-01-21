6a8f52c1448bbb8864c1a60772a8d80f43b0c084
Rafał Pamięta <r.pamieta@gmail.com>
2022-01-21 12:52:53 +0100

    added new section services

--- js/script.js
+++ js/script.js
@@ -3,12 +3,25 @@ const barsBtn = document.querySelector(".fa-bars");
 const xBtn = document.querySelector(".fa-times");
 const menuList = document.querySelector(".menuList");
 const menuItems = document.querySelectorAll("li a");
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
+const accordeon = document.querySelectorAll(".produkt");
 
+const showServ = () => {
+	accordeon.forEach(acc => {
+		acc.addEventListener("click", () => {
+
+			acc.classList.toggle("active");
+
+		
+		});
+	});
+};
+
+showServ();
 
 const handleNav = () => {
-	nav.classList.toggle('move', window.scrollY > 0)
-}
+	nav.classList.toggle("move", window.scrollY > 0);
+};
 
 const showMenu = () => {
 	xBtn.classList.toggle("hide");
@@ -16,9 +29,9 @@ const showMenu = () => {
 	menuList.classList.toggle("showOptions");
 
 	menuItems.forEach(item => {
-		item.addEventListener('click',showMenu);
+		item.addEventListener("click", showMenu);
 	});
 };
 
-window.addEventListener('scroll',handleNav);
+window.addEventListener("scroll", handleNav);
 menuBtn.addEventListener("click", showMenu);
